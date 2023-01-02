d9678667c12a548fc0972e97d9e5d4125c1f1c33
spatiumstories <spatium.nfts@gmail.com>
2023-01-02 08:26:14 -0500

    switching to correct api

--- src/components/Payments/QRCodePayment.js
+++ src/components/Payments/QRCodePayment.js
@@ -133,8 +133,8 @@ const QRCodePayment = (props) => {
             data.append("access_sig", props.buyer.accessSignature);
             data.append("tx_spending_limit", props.buyer.transactionSpendingLimitHex);
             data.append("deposit_tx", depositTx);
-            // let uri = 'https://api.spatiumstories.xyz/api';
-            let uri = 'http://0.0.0.0:4201/api';
+            let uri = 'https://api.spatiumstories.xyz/api';
+            // let uri = 'http://0.0.0.0:4201/api';
             if (props.type === "RARE") {
                 data.append("random_mint", "true");
                 data.append("serial_number", props.serial);
